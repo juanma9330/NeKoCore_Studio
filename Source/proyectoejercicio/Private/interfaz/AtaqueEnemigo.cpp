48e99755b738b3cf27042e437978f4a83a67aa49
juanma9330 <juanmacabral9@gmail.com>
2025-09-21 20:08:28 -0300

    ia enemigo,funcion salto y creado mapa apara test room
    falta conectar funciones a behavior tree y terminar la estructura del enemigo mele
    se agrego un salto simple que hay que pasarlo a c++ y testearlo
    se agrego un nuevo mapa para testoom

---
+++ Source/proyectoejercicio/Private/interfaz/AtaqueEnemigo.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "interfaz/AtaqueEnemigo.h"
+
+// Add default functionality here for any IAtaqueEnemigo functions that are not pure virtual.
